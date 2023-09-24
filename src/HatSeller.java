4c03c761e0a2541b0c22f24d0c00f218e394a0ed
kangwongu <kwg0527@naver.com>
2023-09-24 16:38:41 +0900

    04. 역할, 책임, 협력. 재판과정 (협력) 수정 2

--- src/HatSeller.java
+++ src/HatSeller.java
@@ -5,8 +5,11 @@ public class HatSeller {
         return "HatSeller";
     }
 
-    // 5. 왕의 요청에 응답해 증언한다
     public void testify() {
         System.out.println(this + " 증언합니다.");
+    }
+
+    public HatSeller comeToCourt() {
+        return this;
     }
 }
--- src/WhiteRabbit.java
+++ src/WhiteRabbit.java
@@ -11,10 +11,10 @@ public class WhiteRabbit {
         return "WhiteRabbit";
     }
 
-    // 3. 토끼는 왕의 요청에 응답해 증인을 불러온다
     public HatSeller bringWitness() {
         System.out.println("3. " + this + " -> " + hatSeller + ": 증인석에 입장하라");
         System.out.println(hatSeller + " 입장합니다");
-        return hatSeller;
+        // 3. 토끼는 왕의 요청에 응답해 증인에게 출석을 요청한다
+        return hatSeller.comeToCourt();
     }
 }
